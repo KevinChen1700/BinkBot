074da5c273a6a7749f4be7786a66641e573d6fe7
sjimmie bleeker <blee1703@student.nhl.nl>
2019-05-13 13:44:36 +0200

    shithub xddddd

--- RoboticaGroep3/venv/Sensor.py
+++ RoboticaGroep3/venv/Sensor.py
@@ -1,3 +1,20 @@
-from abc import ABC, abstractmethod
-class Sensor(ABC):
-    data
+class Sensor:
+
+    def __init__(self, trigger, echo):
+        self.trigger = trigger
+        self.echo = echo
+        GPIO.setup(trigger, GPIO.OUT)
+        GPIO.setup(echo, GPIO.IN)
+        GPIO.output(trigger, GPIO.LOW)
+
+    def calcDistance(self):
+        GPIO.output(self.trigger, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.trigger, GPIO.LOW)
+        while GPIO.input(self.echo) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(self.echo) == 1:
+            pulse_end_time = time.time()
+        pulse_duration = pulse_end_time - pulse_start_time
+        return round(pulse_duration * 17150, 2)
+
