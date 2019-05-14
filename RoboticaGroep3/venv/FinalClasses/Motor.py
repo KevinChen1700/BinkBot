4ef8c6b5ee324f61c3a1a54678bf87d25b899b4a
sjimmie bleeker <blee1703@student.nhl.nl>
2019-05-14 13:56:53 +0200

    nieue off

--- RoboticaGroep3/venv/FinalClasses/Motor.py
+++ RoboticaGroep3/venv/FinalClasses/Motor.py
@@ -11,20 +11,16 @@ class Motor:
         self.INA = INA
         self.INB = INB
         GPIO.setup(PWM, GPIO.OUT)
-        pi.set_mode(PWM, pigpio.OUTPUT)
+        self.pi.set_mode(PWM, pigpio.OUTPUT)
         GPIO.setup(INA, GPIO.OUT)
         GPIO.setup(INB, GPIO.OUT)
-        self.on = false
+        self.pi.set_PWM_frequency(self.PWM, 100)
 
-    def onOff(self):
-        if self.on:
-            pi.set_PWM_frequency(PWM, 100)
-        else:
-            pi.set_PWM_frequency(PWM, 0)
-        self.on = not on
+    def off(self):
+            self.pi.set_PWM_dutycycle(self.PWM, 0)
 
     def move(self, right, left, speed):
         GPIO.output(self.INA, right)  # ina op 1 instellen inb op 0, zodat motor naar rechts draait
         GPIO.output(self.INB, left)
         sleep(0.01)  # wait 10ms
-        self.pi.set_PWM_dutycycle(PWM, speed)  # nieuwe snelheid als pwm instellen
+        self.pi.set_PWM_dutycycle(self.PWM, speed)  # nieuwe snelheid als pwm instellen
