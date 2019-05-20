d5da9129713ed1a12e06cca84c87f08f9fc59140
Janvdtuin1 <32636731+Janvdtuin1@users.noreply.github.com>
2019-05-20 11:58:22 +0200

    janbranch push Remote

---
+++ RoboticaGroep3/venv/remote/Remote.py
@@ -0,0 +1,33 @@
+import socket
+#import RPi.GPIO as GPIO
+
+class Remote:
+    __instance = None
+    @staticmethod
+    def getInstance():
+        if Remote.__instance == None:
+            Remote()
+        return Remote.__instance
+
+    def __init__(self):
+        if Remote.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Remote.__instance = self
+            HOST = "141.252.230.54"
+            PORT = 5002
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #this.s.connect((HOST, PORT))
+            self.needsUpdate = False
+            self.lastpressed = "test"
+
+    def getSignal(self):
+        temp = self.s.recv(50)
+        if self.lastpressed == temp:
+            self.needsUpdate = True
+            self.lastpressed = temp
+
+
+
+
+
---
+++ RoboticaGroep3/venv/remote/remotetest.py
@@ -0,0 +1,10 @@
+from Remote import Remote
+
+
+remote = Remote()
+remote2 = remote.getInstance()
+
+remote.getSignal()
+
+print(remote)
+print(remote2)
