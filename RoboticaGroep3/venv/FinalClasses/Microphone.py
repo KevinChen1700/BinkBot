9e734141da58cbd316ef7fdbc1fc8ed1ae720548
chen1700 <32311046+chen1700@users.noreply.github.com>
2019-06-07 11:05:24 +0200

    class werkend

--- RoboticaGroep3/venv/FinalClasses/Microphone.py
+++ RoboticaGroep3/venv/FinalClasses/Microphone.py
@@ -15,52 +15,47 @@ class Microphone:
         if Microphone.__instance != None:
             raise Exception("This class is a singleton!")
         else:
-
-            #self.swt_channel = swt_channel
-            #self.vrx_channel = vrx_channel
-            #self.vry_channel = vry_channel
-            #self.vrb_channel = vrb_channel
-
+            Microphone.__instance = self
+            #lage tonen channel 0
             self.swt_channel = 0
+
+            #midden tonen channel 1
             self.vrx_channel = 1
+
+            #hoge tonen channel 2
             self.vry_channel = 2
+
+            #accu spanning channel 3
             self.vrb_channel = 3
 
 
             # Time delay, which tells how many seconds the value is read out
-            self.delay = 0.5
+            self.delay = 0.01
 
             # Spi oeffnen
-            spi = spidev.SpiDev()
-            spi.open(0, 0)
-            spi.max_speed_hz = 1000000
+            self.spi = spidev.SpiDev()
+            self.spi.open(0, 0)
+            self.spi.max_speed_hz = 1000000
 
     # Function for reading the MCP3008 channel between 0 and 7
     def readChannel(self,channel):
-        self.val = spi.xfer2([1, (8 + self.channel) << 4, 0])
-        data = ((self.val[1] & 3) << 8) + self.val[2]
+        val = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((val[1] & 3) << 8) + val[2]
         return data
 
     #Function for displaying tonen
     def readValue (self):
-        self.vrx_pos = readChannel(self.vrx_channel)
-        self.vry_pos = readChannel(self.vry_channel)
-        self.vrb_pos = readChannel(self.vrb_channel)
+        self.vrx_pos = self.readChannel(self.vrx_channel)
+        self.vry_pos = self.readChannel(self.vry_channel)
+        self.vrb_pos = self.readChannel(self.vrb_channel)
 
         # SW determine
-        self.swt_val = readChannel(self.swt_channel)
-        datastring = ("VRx : {}  VRy : {}  SW : {} Accu : {}".format(self.vrb_pos, self.vrx_pos, self.vry_pos, self.swt_val))
-        print datastring
+        self.swt_val = self.readChannel(self.swt_channel)
+        #self.datastring = ("VRx : {}  VRy : {}  SW : {} Accu : {}".format(self.vrb_pos, self.vrx_pos, self.vry_pos, self.swt_val))
+        self.datastring = ("SW : {}  VRx : {}  VRy : {} Accu : {}".format(self.swt_val, self.vrx_pos, self.vry_pos, self.vrb_pos))
+        print self.datastring
 
 
         # wait
         time.sleep(self.delay)
-
-    # endless loop to test
-    while True:
-        readValue()
-
-
-
-
 
