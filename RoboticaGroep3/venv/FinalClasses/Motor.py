be7160ad3e41ddaa3c00d3010c36721b7efbd382
sjimmie bleeker <blee1703@student.nhl.nl>
2019-06-07 12:44:20 +0200

    acceleratie

--- RoboticaGroep3/venv/FinalClasses/Motor.py
+++ RoboticaGroep3/venv/FinalClasses/Motor.py
@@ -16,21 +16,29 @@ class Motor:
         GPIO.setup(self.INB, GPIO.OUT)
         self.pi.set_PWM_frequency(self.PWM, 100)
         self.speed = 0
-        
+        self.direction = "None"
 
     def off(self):
         self.pi.set_PWM_dutycycle(self.PWM, 0)
 
     def move(self, direction, speed):
         speed = speed * 2.55
-        
+
         if (speed > 0) and (speed <= 80): # speeds between 0 and 80 are unsafe
             speed = 0
-        if (direction == "right") or (direction == "r"):
+
+        if self.direction != direction:
+            self.speed = 0
+
+        if (self.speed < 255) and (self.speed < speed):
+            self.speed += speed * 0.01
+
+        if direction == "right":
             GPIO.output(self.INA, 1)
             GPIO.output(self.INB, 0)
-        elif (direction == "left") or (direction == "l"):
+
+        elif direction == "left":
             GPIO.output(self.INA, 0)
             GPIO.output(self.INB, 1)
         sleep(0.01)  # wait 10ms
-        self.pi.set_PWM_dutycycle(self.PWM, speed)
+        self.pi.set_PWM_dutycycle(self.PWM, self.speed)
