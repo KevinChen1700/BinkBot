cf1f1863f080ba6abc750f45c883e188252c045c
Kevin HBO-ICT <chen1700@student.nhl.nl>
2019-06-11 11:42:57 +0200

    battery met sockets

--- RoboticaGroep3/venv/FinalClasses/Remote.py
+++ RoboticaGroep3/venv/FinalClasses/Remote.py
@@ -1,5 +1,5 @@
 import socket
-#import RPi.GPIO as GPIO
+from Microphone import Microphone
 
 class Remote:
     __instance = None
@@ -13,6 +13,20 @@ class Remote:
         if Remote.__instance != None:
             print("Singleton class already has an instance")
         else:
+            #connection to send data to remote
+            self.s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.s2.connect(("8.8.8.8", 80))
+            self.HOST = self.s2.getsockname()[0]
+            self.PORT = 5002
+            print self.HOST
+            self.s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s2.bind((self.HOST, self.PORT))
+            self.s2.listen(1)
+            self.conn, self.addr = self.s2.accept()
+            self.s2.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            print 'Connected by', self.addr
+
+            #connection to receive data from remote
             Remote.__instance = self
             self.HOST = "141.252.29.24"
             self.PORT = 5002
@@ -27,6 +41,12 @@ class Remote:
             self.needsUpdate = True
             self.lastpressed = temp
         return self.lastpressed
+
+    def sendBatteryValue(self):
+        value = str(self.Microphone.getBattery())
+        self.conn.send(value)
+        print(value)
+
 
 
 
